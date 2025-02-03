d31651b049460e613fff1e5df03874394a74af8f
Gal Oz <galoz05@gmail.com>
2025-02-03 11:01:22 +0200

    ✨ Integrate bot command handling into socket connection logic

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import { createServer } from "http";
 import { Server, Socket } from "socket.io";
 import cors from "cors";
 import connectDB from "./config/mongo";
+import { handleBotCommands } from "./controllers/botController";
 
 const app: Application = express();
 const server = createServer(app);
@@ -16,12 +17,7 @@ app.use(cors());
 app.use(express.json());
 connectDB();
 
-io.on("connection", (socket: Socket) => {
-    console.log(`a user connected ${socket.id}`);
-    socket.on("disconnect", () => {
-      console.log(`a user disconnected ${socket.id}`);
-    });
-  });
+io.on("connection", (socket: Socket) => handleBotCommands(io, socket));
 
 
 export default server;
